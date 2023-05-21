65d5d2e5a89a528104ad9edf962397b0dd1db1d8
patrick.almeida <patricksalmeida@gmail.com>
2023-05-21 16:02:52 -0300

    initial structure

---
+++ src/main/java/br/com/patrickalmeida/apimultithreadingjava/ApiMultithreadingJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrickalmeida.apimultithreadingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMultithreadingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMultithreadingJavaApplication.class, args);
+	}
+
+}
